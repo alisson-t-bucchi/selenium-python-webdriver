9d57f3edf2c6d1e6e6e0ba7e8805f725d7664ac4
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-12-13 21:18:27 +0000

    including empty page when nothing was bought

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -7,8 +7,12 @@ class CheckoutPage(BasePage):
         self.driver = conftest.driver
         self.checkout_page = (By.XPATH, "//a[@href='/checkout' and text()='Checkout']")
         self.confirm_checkout_page = (By.XPATH, "//h4[text()='Thanks for your order!']")
+        self.empty_chechout_page = (By.XPATH, "//h2[text()='Add items to the cart']")
 
     def full_checkout_page(self):
         self.find_element(self.checkout_page)
         self.click(self.checkout_page)
         self.wait_element(self.confirm_checkout_page)
+
+    def empty_checkout_page(self):
+        self.wait_element(self.empty_chechout_page)
